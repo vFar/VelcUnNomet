275ecdc695f59979abc4726c0c8d6d5171d29aca
Markuss <markussdove@gmail.com>
2022-03-08 14:18:36 +0200

    Pievienoti un sākti rakstīti skripti par objektiem un drag un drop, importēti visi faili Editorā

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	//Velkajam objektam piestiprinam komponenti
+	private CanvasGroup grupa;
+	private RectTransform velkObjRectTransform;   
+	public Objekti objektuSkripts;
+
+	void Start () {
+		//Piekĺúst objekta piestiprinatajai CanvasGroup komponentei
+		grupa = GetComponent<CanvasGroup>();
+		velkObjRectTransform = GetComponent<RectTransform> ();
+	}
+
+
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Click on a object");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Begin drag");
+		//Velkot objektu, tas paliek caurspidigs pa 0.6 vienibam, izsledzas blocksraycast
+		grupa.alpha = 0.6f;
+		grupa.blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log ("Object drag");
+		//maina x un y koordinatas
+		velkObjRectTransform.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Stopped dragging");
+	}
+}
